38958e0c7f4b159a4dc21eb767d6fbe36c49b07c
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-09-18 09:57:52 -0500

    Rename sys.IsTty to sys.Isatty to better reflect its origin.
    Also add sys.IsTty that does about the same thing only
    using ioctl and TCGETS to do its jazz.

--- tty.go
+++ tty.go
@@ -1,15 +1,38 @@
 package sys
 
 import (
+	"syscall"
+	"unsafe"
 	// #include <unistd.h>
 	"C"
 )
 
 /*
-IsTty just returns a bool if the fd pointer passed happens to be a tty or not.
+Isatty just returns a bool if the fd pointer passed happens to be a tty or not.
 
 TODO: return errno as well?
 */
-func IsTty(fd uintptr) bool {
+func Isatty(fd uintptr) bool {
 	return int(C.isatty(C.int(fd))) != int(0)
+}
+
+/*
+Termios struct for using TCGETS to see if we havea  tty.
+*/
+type Termios struct {
+	Iflag, Oflag, Cflag, Lflag uint32
+	Cc                         [20]byte
+	Ispeed, Ospeed             uint32
+}
+
+/*
+Fill up a Termios struct with a ioctl call to TCGETS to determine tty status.
+*/
+func IsTty(fd uintptr) bool {
+	var termios Termios
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL,
+		fd,
+		uintptr(syscall.TCGETS),
+		uintptr(unsafe.Pointer(&termios)))
+	return err == 0
 }
